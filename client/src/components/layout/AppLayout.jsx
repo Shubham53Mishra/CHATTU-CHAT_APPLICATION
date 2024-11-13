30efa2eaed14eec4f9a0b4bbfc40f151309e2cf3
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-14 01:11:14 +0530

    create the Tittle

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 const AppLayout = (WrappedComponent) => {
     return (props) => {
         return (
             <div>
-                <div>Header</div>
+                <Header />
                 <WrappedComponent {...props} />
                 <div>Footer</div>
             </div>
