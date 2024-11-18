2b33254ba580aff61db43b4699e28fbd0ef8a509
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-18 23:31:37 +0530

    perfect the color

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 import { AppBar, Box, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { orange } from '../../constants/color'
 
 const Header = () => {
   return <>
     <Box sx={{ flexGrow: 1 }} height={"4rem"}>
       <AppBar position="static" sx={
         {
-          bgcolor: "orange"
+          bgcolor: orange
         }
       }>
         <Toolbar>
