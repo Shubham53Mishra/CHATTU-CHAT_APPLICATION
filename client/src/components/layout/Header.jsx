fd8e5317e455a8417ccaba1914df5470092c2e21
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-22 23:08:23 +0530

    tootkit add

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,33 +1,70 @@
 /* eslint-disable no-unused-vars */
-import { AppBar, Box, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import { orange } from '../../constants/color'
+import { AppBar, Box, Toolbar, Tooltip, Typography } from '@mui/material';
+import React from 'react';
+import { orange } from '../../constants/color';
+import { IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AddIcon from '@mui/icons-material/Search';
 
 const Header = () => {
-  return <>
-    <Box sx={{ flexGrow: 1 }} height={"4rem"}>
-      <AppBar position="static" sx={
-        {
-          bgcolor: orange
-        }
-      }>
-        <Toolbar>
-          <Typography
-            variant="h6"
-            sx={{
-              display: { xs: 'none', sm: 'block' },
+  const handleMobile = () => {
+    console.log("mobile");
+  };
+
+  const openSearchDialog = () => {  
+    console.log("openSearchDialog");
+  };
+  const openNewGroup = () => {  
+    console.log("openNewGroup");
+  };
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar
+          position="static"
+          sx={{
+            bgcolor: orange,
+          }}
+        >
+          <Toolbar>
+            <Typography
+              variant="h6"
+              sx={{
+                display: { xs: 'none', sm: 'block' },
+              }}
+            >
+              chattu
+            </Typography>
+            <Box
+              sx={{
+                display: { xs: 'block', sm: 'none' },
+              }}
+            >
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box sx={{
+              flexGrow: 1,
 
             }}
-          >
-            chattu
-          </Typography>
-        </Toolbar>
+            />
+            <Box>
+            <IconButton color="inherit" size='large' onClick={openSearchDialog}>
+                <SearchIcon />
+              </IconButton>
+             <Tooltip title="Open new group">
+                <IconButton color="inherit" size='large' onClick={openNewGroup}>
+                <AddIcon />
+              </IconButton>
+             </Tooltip>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
 
-
-      </AppBar>
-
-    </Box>
-  </>
-}
-
-export default Header
+export default Header;
