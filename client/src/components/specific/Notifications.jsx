92a17c71a313ad9948def20cf663bc33bca8c027
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-23 00:16:56 +0530

    createw the floder

---
+++ client/src/components/specific/Notifications.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Notifications = () => {
+  return (
+    <div>Notifications</div>
+  )
+}
+
+export default Notifications
