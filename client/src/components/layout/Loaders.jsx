cb1dbb60ac537ac8f1f7337f64acc459385e4d39
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-23 11:37:14 +0530

    skeleton rectangular

--- client/src/components/layout/Loaders.jsx
+++ client/src/components/layout/Loaders.jsx
@@ -2,21 +2,27 @@ import { Grid,Skeleton } from '@mui/material';
 export const Loaders = () => {
  
   return (
-    <Grid container height={"calc(100vh - 4rem)"}>
+    <Grid container height={"calc(100vh - 4rem)" } spacing={"1rem"}>
     <Grid item  sm={4} md={3}  sx={{ display: { xs: 'none', sm: 'block' } }} height={"100%"} bgcolor="primary.main">
-        first
+    <Skeleton variant='rectangular' height={"100vh"}/>
         {/* Content for the first column */}
     </Grid>
     <Grid item xs={12 } sm={8} md={5} lg={6} height={"100%"} >
         {/* Content for the second column */}
-         <WrappedComponent {...props} />
+        <Skeleton variant='rectangular' height={100}/>
+        <Skeleton variant='rectangular' height={100}/>
+        <Skeleton variant='rectangular' height={100}/>
+        <Skeleton variant='rectangular' height={100}/>
+        <Skeleton variant='rectangular' height={100}/>
+        <Skeleton variant='rectangular' height={100}/>
+        
     </Grid>
-    <Grid item md={4} lg={3} height={"100%"}  sx={{ display: { xs: 'none', md: 'block' }, padding:"2rem", bgcolor:"rgba(0,0,0,0.85)" }}
+    <Grid item md={4} lg={3} height={"100%"}  sx={{ display: { xs: 'none', md: 'block' },  }}
     
 
     >
       
-        third
+      <Skeleton variant='rectangular' height={"100vh"}/>
     </Grid>
    
 </Grid>
