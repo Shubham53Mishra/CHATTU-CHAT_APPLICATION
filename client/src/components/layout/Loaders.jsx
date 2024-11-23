f17997fc6e26a56d57b45b57f55f6834728b48dc
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-23 11:44:26 +0530

    Array map

--- client/src/components/layout/Loaders.jsx
+++ client/src/components/layout/Loaders.jsx
@@ -1,4 +1,4 @@
-import { Grid,Skeleton } from '@mui/material';
+import { Grid,Skeleton, Stack } from '@mui/material';
 export const Loaders = () => {
  
   return (
@@ -7,15 +7,15 @@ export const Loaders = () => {
     <Skeleton variant='rectangular' height={"100vh"}/>
         {/* Content for the first column */}
     </Grid>
-    <Grid item xs={12 } sm={8} md={5} lg={6} height={"100%"} >
-        {/* Content for the second column */}
-        {
-          Array.from({ length: 10 }).map((_, index) => (
-            <Skeleton key={index} variant='rectangular' height={"5rem"}/>
-          ))
-        }
+    <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
+  {/* Content for the second column */}
+  <Stack spacing={"1rem"}>
+    {Array.from({ length: 10 }).map((_, index) => (
+      <Skeleton key={index} variant="rectangular" height="5rem" />
+    ))}
+  </Stack>
+</Grid>
 
-    </Grid>
     <Grid item md={4} lg={3} height={"100%"}  sx={{ display: { xs: 'none', md: 'block' },  }}
     
 
