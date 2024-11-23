a4773d7e684e8e67cf9c8fe1acdef3d61911a11b
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-23 12:10:35 +0530

    chatID

--- client/src/components/shared/ChatItem.jsx
+++ client/src/components/shared/ChatItem.jsx
@@ -1,16 +1,40 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from 'react';
 import { Stack } from '@mui/material';
 
-const Chatitem = ( {w="100%",chats=[],chatId,onlineUsers=[],newMessagesAlert=[{
-    chatId:1,
-    count:0
-}]}) => {
+const Chatitem = ({
+  w = "100%", // Default width
+  chats = [], // List of chats
+  chatId, // Current chat ID
+  onlineUsers = [], // List of online users
+  newMessagesAlert = [
+    {
+      chatId: 1,
+      count: 0, // Default alert for messages
+    },
+  ],
+  handleDeleteChat, // Function to handle chat deletion
+}) => {
   return (
-    <Stack>
-
+    <Stack width={w} direction="column">
+      {chats.length > 0 ? (
+        chats.map((chat, index) => (
+          <div key={chat.id || index}>
+            <p>{chat.name || `Chat ${index + 1}`}</p>
+            {newMessagesAlert.find((alert) => alert.chatId === chat.id) && (
+              <span>
+                {newMessagesAlert.find((alert) => alert.chatId === chat.id).count} new
+                messages
+              </span>
+            )}
+            <button onClick={() => handleDeleteChat(chat.id)}>Delete Chat</button>
+          </div>
+        ))
+      ) : (
+        <p>No chats available</p>
+      )}
     </Stack>
-  )
-}
+  );
+};
 
-export default Chatitem
+export default Chatitem;
