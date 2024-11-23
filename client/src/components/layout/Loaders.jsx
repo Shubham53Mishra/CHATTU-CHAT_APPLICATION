cdc9caba3bc15e04c6edcc06ae51846bf7952fb3
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-23 11:40:32 +0530

    Array map

--- client/src/components/layout/Loaders.jsx
+++ client/src/components/layout/Loaders.jsx
@@ -9,13 +9,12 @@ export const Loaders = () => {
     </Grid>
     <Grid item xs={12 } sm={8} md={5} lg={6} height={"100%"} >
         {/* Content for the second column */}
-        <Skeleton variant='rectangular' height={100}/>
-        <Skeleton variant='rectangular' height={100}/>
-        <Skeleton variant='rectangular' height={100}/>
-        <Skeleton variant='rectangular' height={100}/>
-        <Skeleton variant='rectangular' height={100}/>
-        <Skeleton variant='rectangular' height={100}/>
-        
+        {
+          Array.from({ length: 10 }).map((_, index) => (
+            <Skeleton key={index} variant='rectangular' height={"5rem"}/>
+          ))
+        }
+
     </Grid>
     <Grid item md={4} lg={3} height={"100%"}  sx={{ display: { xs: 'none', md: 'block' },  }}
     
