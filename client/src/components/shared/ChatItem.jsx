1c44c01b413a42eda5905aac9604a318faafa789
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-11-24 12:21:40 +0530

    nice to comeback sir

--- client/src/components/shared/ChatItem.jsx
+++ client/src/components/shared/ChatItem.jsx
@@ -1,4 +1,4 @@
-import { Avatar } from '@mui/material'
+import { Avatar, Stack, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from  '../styles/StyledCompoents.jsx'
 
@@ -9,13 +9,31 @@ const Chatitem = () => {
         groupChat = false,
         someSender,
          isOnline
-         newMessages,
+         newMessagesAlert,
          index=0,
          handleDeleteChatOpen,
     return (
-       <Link to={"/chat/chat/${_id}"}>
-        <div style={{display:"flex", gap:"1rem", position:"relative",      alignItems:"center", padding:"1rem", backgroundColor: sameSender ? "black" : "upset",color:"samesender" ?"white" :"black", justifyContent:"space-between"}}>
-
+       <Link to={"/chat/chat/${_id}"} onContextMenu={(e) => handleDeleteChatOpen(e,_id,groupChat)}>
+        <div style={{display:"flex", gap:"1rem", position:"relative", alignItems:"center", padding:"1rem", backgroundColor: sameSender ? "black" : "upset",color:"samesender" ?"white" :"black", justifyContent:"space-between"}}>
+            <Stack>
+                <Typography>{name}</Typography>
+                {
+                    newMessagesAlert && (
+                        <Typography>{newMessagesAlert.count}New Messages</Typography> 
+                    )
+                }
+            </Stack>
+            {
+                isOnline && <Box{{
+                    position:"absolute",
+                    top:"0",
+                    right:"0",
+                    width:"1rem",
+                    height:"1rem",
+                    backgroundColor:"green",
+                    borderRadius:"50%"
+                }}/>
+            }
         </div>
        </Link>
     )
